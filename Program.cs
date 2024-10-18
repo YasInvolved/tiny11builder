ad3d228efed36ff1cfe9eef0ccd89e74e809f0b1
YasInvoled <szrtxm.op@gmail.com>
2024-10-18 22:33:58 +0200

    api interactions with uupdump.net and necessary types

---
+++ Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace tiny11builder
+{
+    public static class Utils
+    {
+        public static async Task<List<Language>> GetLanguages(string versionId)
+        {
+            List<Language> languages = new List<Language>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"{Constants.UUPDUMP_URL}/listlangs.php?id={versionId}");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject jsonObject = JObject.Parse(responseBody);
+                JObject langFancyNames = (JObject)jsonObject["response"]["langFancyNames"];
+
+                foreach (var entry in langFancyNames)
+                {
+                   languages.Add(new Language(entry.Key, entry.Value.ToString()));
+                }
+            }
+
+            return languages;
+        }
+
+        public static async Task<List<Edition>> GetEditions(string versionId)
+        {
+            List<Edition> editions = new List<Edition>();
+
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync($"{Constants.UUPDUMP_URL}/listeditions.php?id={versionId}");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                JObject jsonObject = JObject.Parse(responseBody);
+                JObject editionFancyNames = (JObject)jsonObject["response"]["editionFancyNames"];
+
+                foreach (var entry in editionFancyNames)
+                {
+                    editions.Add(new Edition(entry.Value.ToString(), entry.Key));
+                }
+            }
+
+            return editions;
+        }
+
+        public static async Task<Update> GetUpdate(string versionId, Language lang, Edition edition)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync($"{Constants.UUPDUMP_URL}/get.php?id={versionId}&lang={lang.LangCode}&edition={edition.Code}"))
+                {
+                    response.EnsureSuccessStatusCode();
+                    JObject body = (JObject)JObject.Parse(await response.Content.ReadAsStringAsync())["response"];
+
+                    string updateName = (string)body["updateName"];
+                    string arch = (string)body["arch"];
+                    string build = (string)body["build"];
+
+                    List<UpdateFile> files = new List<UpdateFile>();
+
+                    foreach (var file in body["files"].Children<JProperty>())
+                    {
+                        if (file.Name.EndsWith(".cab"))
+                        {
+                            files.Add(new CabinetFile(
+                                file.Name, 
+                                $"updatefiles/{file.Name}",
+                                (string)file.Value["url"],
+                                (string)file.Value["sha1"], 
+                                (int)file.Value["size"], 
+                                (string)file.Value["uuid"], 
+                                (string)file.Value["debug"]
+                            ));
+                        }
+                    }
+
+                    return new Update(updateName, arch, build, files);
+                }
+            }
+        }
+
+        public static T GetInputForDictionary<T>(string text, Dictionary<string, T> options)
+        {
+            while (true)
+            {
+                Console.WriteLine($"{text}");
+                foreach (var pair in options)
+                {
+                    Console.WriteLine($"- {pair.Key}");
+                }
+
+                Console.Write("\n\nPlease choose an option: ");
+                string input = Console.ReadLine().ToUpper();
+
+                if (options.ContainsKey(input)) return options[input];
+                Console.WriteLine("Wrong value inserted. Please retry.\n\n\n\n");
+                Thread.Sleep(2000);
+            }
+        }
+
+        public static T GetInputForArray<T>(string text, List<T> options)
+        {
+            while (true)
+            {
+                Console.WriteLine($"{text}");
+                for (int i = 0; i < options.Count; i++)
+                {
+                    Console.WriteLine($"{i}: {options[i]}");
+                }
+
+                Console.Write("\n\nPlease choose an index: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out int value) && value < options.Count) return options[value];
+                Console.WriteLine("Wrong value inserted. Please retry.\n\n\n\n");
+                Thread.Sleep(2000);
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Tiny11Builder";
+            Console.WriteLine("Welcome to tiny11builder!\n\n\n\n");
+
+            string versionId = Utils.GetInputForDictionary("Select Windows Version: ", Constants.WINDOWS_VERSIONS);
+
+            var languages = Utils.GetLanguages(versionId);
+            var editions = Utils.GetEditions(versionId);
+
+            languages.Wait();
+            Language language = Utils.GetInputForArray("Select the language", languages.Result);
+            Console.WriteLine($"Selected: {language}");
+            
+            editions.Wait();
+            Edition edition = Utils.GetInputForArray("Select desired edition", editions.Result);
+            Console.WriteLine($"Selected: {edition}");
+
+            var update = Utils.GetUpdate(versionId, language, edition);
+            update.Wait();
+
+            Console.WriteLine($"Fetched update.\n{update.Result.UpdateName}\nArch: {update.Result.Arch}\nBuild ID: {update.Result.Build}");
+
+            List<Task<bool>> tasks = new List<Task<bool>>();
+            foreach (var file in update.Result.Files)
+            {
+                tasks.Add(file.Download());
+                if (file is CabinetFile && file.Path.ToLower().Contains("aggregatedmetadata"))
+                {
+                    tasks.Add(((CabinetFile)file).Unpack("metadata"));
+                }
+            }
+            
+            Task.WaitAll(tasks.ToArray());
+            Console.WriteLine("Downloading completed");
+
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
---
+++ UUPDumpTypes.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Net.Http;
+using System.IO;
+
+namespace tiny11builder
+{
+    public class Constants
+    {
+        public static readonly string UUPDUMP_URL = "https://api.uupdump.net";
+        public static readonly Dictionary<string, string> WINDOWS_VERSIONS = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+        {
+            { "23H2", "e93eb708-98ec-494c-b4fc-025c6b012173" },
+            { "22H2", "e7036acd-c5d4-46e5-bce0-ccb55d8dbf56" },
+            { "21H2", "35630b7b-4509-45b6-83a1-d4c75d5aa9b6" }
+        };
+    }
+
+    public class Language
+    {
+        public Language(string langCode, string fancyName)
+        {
+            LangCode = langCode;
+            FancyName = fancyName;
+        }
+
+        public override string ToString()
+        {
+            return FancyName;
+        }
+
+        public string LangCode { get; }
+        public string FancyName { get; }
+    }
+
+    public class Edition
+    {
+        public Edition(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public string Name { get; }
+        public string Code { get; }
+    }
+
+    public class UpdateFile
+    {
+        public UpdateFile(string name, string path, string url, string sha1, int size, string uuid, string debug)
+        {
+            Name = name;
+            Path = path;
+            Url = url;
+            SHA1 = sha1;
+            Size = size;
+            UUID = uuid;
+            Debug = debug;
+        }
+
+        public async Task<bool> Download()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync(Url, HttpCompletionOption.ResponseHeadersRead))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    if (!Directory.Exists("updatefiles")) Directory.CreateDirectory("updatefiles");
+                    if (File.Exists(Path)) return true;
+
+                    using (Stream contentStream = await response.Content.ReadAsStreamAsync(),
+                        fileStream = new FileStream(Path, FileMode.OpenOrCreate, FileAccess.ReadWrite, FileShare.None))
+                    {
+                        await contentStream.CopyToAsync(fileStream);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public string Name { get; }
+        public string Path {  get; }
+        private string Url { get; }
+        public string SHA1 { get; }
+        public int Size { get; }
+        public string UUID { get; }
+        public string Debug { get; }
+    }
+
+    public class CabinetFile : UpdateFile 
+    {
+        public CabinetFile(string name, string path, string url, string sha1, int size, string uuid, string debug)
+            : base(name, path, url, sha1, size, uuid, debug) { }
+
+        public async Task<bool> Unpack(string destination)
+        {
+            try
+            {
+                var processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "expand.exe",
+                    Arguments = $"\"{Path}\" -F:* \"{destination}\"",
+                    RedirectStandardError = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                };
+
+                if (!Directory.Exists(destination)) Directory.CreateDirectory(destination);
+
+                using (var process = Process.Start(processStartInfo))
+                {
+                    string stderr = await process.StandardError.ReadToEndAsync();
+
+                    process.WaitForExit();
+
+                    if (string.IsNullOrEmpty(stderr))
+                    {
+                        Console.WriteLine(stderr);
+                    }
+
+                    return process.ExitCode == 0;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return false;
+            }
+        }
+    }
+
+    public class EsdFile : UpdateFile
+    {
+        public EsdFile(string name, string path, string url, string sha1, int size, string uuid, string debug)
+            : base(name, path, url, sha1, size, uuid, debug) { }
+    }
+
+    public class Update
+    {
+        public Update(string updateName, string arch, string build, List<UpdateFile> files)
+        {
+            UpdateName = updateName;
+            Arch = arch;
+            Build = build;
+            Files = files;
+        }
+
+        public string UpdateName { get; }
+        public string Arch { get; }
+        public string Build { get; }
+        public List<UpdateFile> Files { get; }
+    }
+}
